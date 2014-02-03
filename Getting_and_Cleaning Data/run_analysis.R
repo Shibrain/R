a959439645f8a5bbf2242cfc0f2fdfb53fa1e596
ghromis <ghromis@gmail.com>
2014-02-03 19:55:25 +0100

    new text file

--- Getting_and_Cleaning Data/run_analysis.R
+++ Getting_and_Cleaning Data/run_analysis.R
@@ -1,28 +1,44 @@
 trainSetUrl <- "./UCI HAR Dataset/train/X_train.txt"
 trainLabelUrl <- "./UCI HAR Dataset/train/y_train.txt"
+trainSubjectUrl <- "./UCI HAR Dataset/train/subject_train.txt"
+
 testSetUrl <- "./UCI HAR Dataset/test/X_test.txt"
 testLabelUrl <- "./UCI HAR Dataset/test/y_test.txt"
+testSubjectUrl <- "./UCI HAR Dataset/test/subject_test.txt"
+
+featuresUrl <- "./UCI HAR Dataset/features.txt"
+features <- read.table(featuresUrl, header=F)
 
 trainSet <- read.table(trainSetUrl, header=F)
+colnames(trainSet) <- features$V2
 trainLabel <- read.table(trainLabelUrl, header=F)
-train <- cbind(trainSet,trainLabel)
+trainSubject <- read.table(trainSubjectUrl, header=F)
+train <- cbind(trainSet,trainLabel, trainSubject)
 
 testSet <- read.table(testSetUrl, header=F)
+colnames(testSet) <- features$V2
 testLabel <- read.table(testLabelUrl, header=F)
-test <- cbind(testSet, testLabel)
+testSubject <- read.table(testSubjectUrl, header=F)
+test <- cbind(testSet, testLabel, testSubject)
 
 mergedData <-rbind(train,test)
-colnames(mergedData)[562] <- "label"
+colnames(mergedData)[562] <- "Label"
+colnames(mergedData)[563] <- "Subject"
 allLabels <- c("walking", "walking upstairs", "walking downstairs",
               "sitting", "standing", "laying")
 for (i in 1:6){
-  mergedData$label[mergedData$label== i] <- allLabels[i]
+  mergedData$Label[mergedData$Label== i] <- allLabels[i]
 }
 
-means <- aggregate(. ~ label, data = mergedData, mean)
-write.csv(means, file="means.csv", row.names=FALSE)
-write.table(means, file="means.txt", row.names=FALSE)
-sd <- aggregate(. ~ label, data = mergedData, sd)
+extractedColumns <- grep("mean\\(\\)|std\\(\\)", features$V2)
+extractedValues <- mergedData[c(extractedColumns, 562,563)]
 
-# average <- data.frame(Activity=mean[,1], Average=rowMeans(mean[,-1]))
-# test <- read.table("means.txt", header=T)
+tidySet <- aggregate(. ~ Label+Subject, data = extractedValues, mean)
+
+write.csv(means, file="tidySet.csv", row.names=FALSE)
+write.table(means, file="tidySet.txt", row.names=FALSE)
+
+
+
+
+
